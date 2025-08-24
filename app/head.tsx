1283cd0654b2a79197b9d38141da5e244a9decb6
Sevens Toussaint <ton-email@exemple.com>
2025-08-24 16:11:25 -0400

    chore: ignore .env.local

--- app/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>AI Générateur de Combinaisons</title>
-      <meta name="description" content="Générateur intelligent de combinaisons pour Loto Max, 6/49 et Grande Vie" />
-    </>
-  );
-}
